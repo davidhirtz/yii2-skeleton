7ccc255bc817266843618eeaf7f6d7a07cb69a0b
David Hirtz <david.hirtz@me.com>
2019-07-04 12:39:22 +0200

    jQuery Update

--- composer/Bootstrap.php
+++ composer/Bootstrap.php
@@ -63,7 +63,7 @@ class Bootstrap implements BootstrapInterface
                         'yii\web\JqueryAsset' => [
                             'sourcePath' => null,
                             'js' => [
-                                '//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js',
+                                '//cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js',
                             ],
                         ],
                         'yii\jui\JuiAsset' => [
