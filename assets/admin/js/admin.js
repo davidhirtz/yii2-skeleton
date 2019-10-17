bc0b1ffe64ac42d724469f43caa8f5ff0f521356
David Hirtz <david.hirtz@me.com>
2019-10-17 13:08:53 +0200

    CKEditor Link fixes

--- assets/admin/js/admin.js
+++ assets/admin/js/admin.js
@@ -111,5 +111,45 @@ $(function () {
                 });
             }
         });
+    };
+
+    if (window.hasOwnProperty('CKEDITOR')) {
+        CKEDITOR.on('dialogDefinition', function (ev) {
+            var dialogName = ev.data.name,
+                dialogDefinition = ev.data.definition;
+
+            if (dialogName === 'link') {
+                var infoTab = dialogDefinition.getContents('info'),
+                    url = infoTab.get('url'),
+                    targetTab = dialogDefinition.getContents('target'),
+                    targetType = targetTab.get('linkTargetType');
+
+                infoTab.remove('protocol');
+                infoTab.remove('emailOptions');
+                infoTab.remove('telOptions');
+                infoTab.remove('anchorOptions');
+                infoTab.remove('browse');
+
+                // Cannot remove linkType without breaking the functionality...
+                infoTab.get('linkType').style = 'display: none';
+
+                url.onKeyUp = function (data) {
+                };
+
+                url.setup = function (data) {
+                    this.allowOnChange = false;
+                    if (data.url) {
+                        this.setValue((typeof data.url.protocol == 'string' ? data.url.protocol : '') + data.url.url);
+                    }
+                    this.allowOnChange = true;
+                };
+                url.commit = function (data) {
+                    data.url = {protocol: '', url: this.getValue()};
+                };
+
+                // Remove useless targets.
+                targetType.items = [targetType.items[0], targetType.items[3]];
+            }
+        });
     }
 });
