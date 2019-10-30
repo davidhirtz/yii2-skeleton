ee5a31cecc9a2e30627ef90bfe00a01928cb3f3c
David Hirtz <david.hirtz@me.com>
2019-10-30 20:44:38 +0100

    Hex Color

--- widgets/bootstrap/ActiveField.php
+++ widgets/bootstrap/ActiveField.php
@@ -2,15 +2,30 @@
 
 namespace davidhirtz\yii2\skeleton\widgets\bootstrap;
 
-use davidhirtz\yii2\skeleton\widgets\forms\ActiveFieldTrait;
+
+use Yii;
 
 /**
- * Class ActiveField.
+ * Class ActiveField
  * @package davidhirtz\yii2\skeleton\widgets\bootstrap
  */
 class ActiveField extends \yii\bootstrap4\ActiveField
 {
-    use ActiveFieldTrait;
+    /**
+     * @var array containing a custom list of languages used for i18n aware attributes.
+     * Leave empty to use default languages.
+     */
+    public $languages;
+
+    /**
+     * @var string input group with appended text.
+     */
+    public $appendInputTemplate = '<div class="input-group">{input}<div class="input-group-append"><span class="input-group-text">{append}</span></div></div>';
+
+    /**
+     * @var string input group with prepended text.
+     */
+    public $prependInputTemplate = '<div class="input-group"><div class="input-group-prepend"><span class="input-group-text">{prepend}</span></div>{input}</div>';
 
     /**
      * @var string
@@ -52,12 +67,54 @@ class ActiveField extends \yii\bootstrap4\ActiveField
      */
     public function dropdownList($items, $options = [])
     {
-        if($items || $this->model->isAttributeRequired($this->attribute)) {
+        if ($items || $this->model->isAttributeRequired($this->attribute)) {
             return parent::dropdownList($items, $options);
         }
 
         // Don't render empty drop down list.
         $this->parts['{input}'] = null;
         return $this;
+    }
+
+    /**
+     * @param string $text
+     * @return $this
+     */
+    public function appendInput($text)
+    {
+        $this->inputTemplate = strtr($this->appendInputTemplate, ['{append}' => $text]);
+        return $this;
+    }
+
+    /**
+     * @param string $text
+     * @return $this
+     */
+    public function prependInput($text)
+    {
+        $this->inputTemplate = strtr($this->prependInputTemplate, ['{prepend}' => $text]);
+        return $this;
+    }
+
+    /**
+     * @param array $options
+     * @return $this
+     */
+    public function hexColor($options = [])
+    {
+        $options['maxlength'] = 6;
+        return $this->input('text', $options)->prependInput('#');
+    }
+
+    /**
+     * @param array $options
+     * @return $this
+     */
+    public function slug($options = [])
+    {
+        $baseUrl = rtrim($options['baseUrl'] ?? Yii::$app->getRequest()->getHostInfo(), '/') . '/';
+        unset($options['baseUrl']);
+
+        return $this->input('text', $options)->prependInput($baseUrl);
     }
 }
