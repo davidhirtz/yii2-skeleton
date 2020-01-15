aaec77f94add45dc399759bc30f56fdad2682387
David Hirtz <david.hirtz@me.com>
2020-01-15 21:11:42 +0100

    Create MaterializedTreeTrait.php

---
+++ db/MaterializedTreeTrait.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace davidhirtz\yii2\skeleton\db;
+
+use davidhirtz\yii2\skeleton\helpers\ArrayHelper;
+
+/**
+ * Class MaterializedTreeTrait
+ * @package davidhirtz\yii2\skeleton\db
+ *
+ * @property int $id
+ * @property int $parent_id
+ * @property int $rgt
+ * @property int $lft
+ * @property string $name
+ *
+ * @property ActiveRecord $parent
+ * @method static ActiveQuery find()
+ */
+trait MaterializedTreeTrait
+{
+    /**
+     * @var ActiveRecord[]
+     * @see getAncestors()
+     */
+    private $_ancestors;
+
+    /**
+     * @var ActiveRecord[]
+     * @see getDescendants()
+     */
+    private $_descendants;
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getParent()
+    {
+        return $this->hasOne(static::class, ['id' => 'parent_id']);
+    }
+
+    /**
+     * @param bool $refresh
+     * @return ActiveRecord[]
+     */
+    public function getAncestors($refresh = false)
+    {
+        if ($this->_ancestors === null || $refresh) {
+            $this->_ancestors = $this->findAncestors()
+                ->indexBy('id')
+                ->all();
+        }
+
+        return $this->_ancestors;
+    }
+
+    /**
+     * @param ActiveRecord[]|null $ancestors
+     */
+    public function setAncestors($ancestors)
+    {
+        $this->_ancestors = [];
+
+        if ($this->path) {
+            $locationIds = ArrayHelper::cacheStringToArray($this->path);
+            foreach ($locationIds as $locationId) {
+                if (isset($ancestors[$locationId])) {
+                    $this->_ancestors[$locationId] = $ancestors[$locationId];
+                }
+            }
+        }
+    }
+
+    /**
+     * @return ActiveRecord|null
+     */
+    public function getFirstAncestor()
+    {
+        if ($this->parent_id) {
+            $ancestors = $this->getAncestors();
+            return current($ancestors);
+        }
+
+        return null;
+    }
+
+    /**
+     * @return ActiveQuery
+     */
+    public function findAncestors()
+    {
+        return static::find()->where(['id' => ArrayHelper::cacheStringToArray($this->path)])
+            ->orderBy(['path' => SORT_ASC]);
+    }
+
+    /**
+     * @param bool $refresh
+     * @return ActiveRecord[]
+     */
+    public function getDescendants($refresh = false)
+    {
+        if ($this->_descendants === null || $refresh) {
+            $this->_descendants = $this->findDescendants()
+                ->indexBy('id')
+                ->all();
+        }
+
+        return $this->_descendants;
+    }
+
+    /**
+     * @param static[] $descendants
+     */
+    public function setDescendants($descendants)
+    {
+        $this->_descendants = [];
+        $length = strlen($this->path);
+
+        foreach ($descendants as $descendant) {
+            if (substr($descendant->path, 0, $length) === $this->path) {
+                $this->_descendants[$descendant->id] = $descendant;
+            }
+        }
+    }
+
+    /**
+     * @return ActiveQuery
+     */
+    public function findDescendants()
+    {
+        $path = ArrayHelper::createCacheString(ArrayHelper::cacheStringToArray($this->path, $this->id));
+        return static::find()->where(static::tableName() . '.[[path]] LIKE :path', ['path' => $path . '%'])
+            ->orderBy(['path' => SORT_ASC]);
+    }
+}
