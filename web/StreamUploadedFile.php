64bed1f27894b0aa5ea76701617f835a723efd79
davidhirtz <david.hirtz@me.com>
2020-11-05 09:35:02 +0100

    Update StreamUploadedFile.php

--- web/StreamUploadedFile.php
+++ web/StreamUploadedFile.php
@@ -3,6 +3,7 @@
 namespace davidhirtz\yii2\skeleton\web;
 
 use davidhirtz\yii2\skeleton\helpers\FileHelper;
+use yii\web\UploadedFile;
 
 /**
  * Class StreamUploadedFile
@@ -10,17 +11,12 @@ use davidhirtz\yii2\skeleton\helpers\FileHelper;
  *
  * @property string $partialName
  */
-class StreamUploadedFile extends \yii\web\UploadedFile
+class StreamUploadedFile extends UploadedFile
 {
     /**
-     * @var
+     * @var string
      */
     public $url;
-
-    /**
-     * @var resource
-     */
-    private $tmpFile;
 
     /**
      * Uploads file from url.
@@ -29,19 +25,15 @@ class StreamUploadedFile extends \yii\web\UploadedFile
     {
         if (!$this->url || !($contents = @file_get_contents($this->url))) {
             $this->error = UPLOAD_ERR_NO_FILE;
+        } elseif (($tmpFile = tmpfile()) === false) {
+            $this->error = UPLOAD_ERR_NO_TMP_DIR;
+        } elseif (fwrite($tmpFile, $contents) === false) {
+            $this->error = UPLOAD_ERR_CANT_WRITE;
         } else {
-            if (($this->tmpFile = tmpfile()) === false) {
-                $this->error = UPLOAD_ERR_NO_TMP_DIR;
-
-            } elseif (fwrite($this->tmpFile, $contents) === false) {
-                $this->error = UPLOAD_ERR_CANT_WRITE;
-
-            } else {
-                $this->name = basename(parse_url($this->url, PHP_URL_PATH));
-                $this->tempName = stream_get_meta_data($this->tmpFile)['uri'];
-                $this->type = FileHelper::getMimeType($this->tempName);
-                $this->size = filesize($this->tempName);
-            }
+            $this->name = basename(parse_url($this->url, PHP_URL_PATH));
+            $this->tempName = stream_get_meta_data($tmpFile)['uri'];
+            $this->type = FileHelper::getMimeType($this->tempName);
+            $this->size = filesize($this->tempName);
         }
 
         parent::init();
