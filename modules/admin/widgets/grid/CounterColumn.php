296eb70bdff26db6dfe8f289394931cea4960e8d
davidhirtz <david.hirtz@me.com>
2021-02-02 15:38:49 +0100

    Create CounterColumn.php

---
+++ modules/admin/widgets/grid/CounterColumn.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace davidhirtz\yii2\skeleton\modules\admin\widgets\grid;
+
+use Yii;
+use yii\base\Model;
+use yii\grid\DataColumn;
+use yii\helpers\Html;
+
+/**
+ * Class CounterColumn
+ * @package davidhirtz\yii2\skeleton\modules\admin\widgets\grid
+ */
+class CounterColumn extends DataColumn
+{
+    /**
+     * @var array
+     */
+    public $headerOptions = ['class' => 'd-none d-md-table-cell text-center'];
+
+    /**
+     * @var array
+     */
+    public $contentOptions = ['class' => 'd-none d-md-table-cell text-center'];
+
+    /**
+     * @var callable|null
+     */
+    public $route;
+
+    /**
+     * @var array
+     */
+    public $numberFormatterOptions = [];
+
+    /**
+     * @var array
+     */
+    public $numberFormatterTextOptions = [];
+
+    /**
+     * @var string[]
+     */
+    public $countHtmlOptions = ['class' => 'badge'];
+
+    /**
+     * @var string
+     */
+    public $emptyValue = '';
+
+    /**
+     * @inheritDoc
+     */
+    public function init()
+    {
+        if (!$this->content) {
+            $this->content = function (Model $model, $key, $index) {
+                if (!($count = $this->getDataCellValue($model, $key, $index))) {
+                    return $this->emptyValue;
+                }
+
+                $count = Yii::$app->getFormatter()->asInteger($count, $this->numberFormatterOptions, $this->numberFormatterTextOptions);
+                $route = $this->route ? call_user_func($this->route, $model) : null;
+
+                return $route ? Html::a($count, $route, $this->countHtmlOptions) : Html::tag('div', $count, $this->countHtmlOptions);
+            };
+        }
+
+        parent::init();
+    }
+}
