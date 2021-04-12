0863d2ffabe88001549b1c2944a86d8c77784d9c
davidhirtz <david.hirtz@me.com>
2021-04-12 19:11:40 +0200

    Added tab method to CKEditor

--- assets/ckeditor/build-config.js
+++ assets/ckeditor/build-config.js
@@ -1,5 +1,5 @@
 ﻿/**
- * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/license
  */
 
@@ -13,10 +13,10 @@
  * (1) https://ckeditor.com/cke4/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) https://ckeditor.com/cke4/builder/9d959f6eb84bac0887abdf42ee7488e4
+ * (2) https://ckeditor.com/cke4/builder/060935482ec7cb65af803926fa27b107
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) https://ckeditor.com/cke4/builder/download/9d959f6eb84bac0887abdf42ee7488e4
+ * (3) https://ckeditor.com/cke4/builder/download/060935482ec7cb65af803926fa27b107
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -39,6 +39,7 @@ var CKBUILDER_CONFIG = {
 		'.jshintrc',
 		'.mailmap',
 		'.npm',
+		'.nvmrc',
 		'.travis.yml',
 		'bender-err.log',
 		'bender-out.log',
@@ -48,6 +49,7 @@ var CKBUILDER_CONFIG = {
 		'gruntfile.js',
 		'less',
 		'node_modules',
+		'package-lock.json',
 		'package.json',
 		'tests'
 	],
@@ -64,6 +66,7 @@ var CKBUILDER_CONFIG = {
 		'list' : 1,
 		'removeformat' : 1,
 		'sourcearea' : 1,
+		'tab' : 1,
 		'table' : 1,
 		'toolbar' : 1,
 		'undo' : 1
