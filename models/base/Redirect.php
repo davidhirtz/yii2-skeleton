640921d39a90b63fe305e02d6e2094f4921379d1
davidhirtz <david.hirtz@me.com>
2021-09-01 18:53:11 +0200

    Update Redirect.php

--- models/base/Redirect.php
+++ models/base/Redirect.php
@@ -159,7 +159,7 @@ class Redirect extends ActiveRecord
      */
     public function getBaseUrl(): string
     {
-        return '/';
+        return !parse_url($this->url, PHP_URL_HOST) ? '/' : '';
     }
 
     /**
