5050864b95d3bb3e1fe5ee88701f9bdabb594ad9
davidhirtz <david.hirtz@me.com>
2021-12-23 17:17:58 +0100

    Update StringHelper.php

--- helpers/StringHelper.php
+++ helpers/StringHelper.php
@@ -2,10 +2,48 @@
 
 namespace davidhirtz\yii2\skeleton\helpers;
 
+use yii\helpers\BaseStringHelper;
+
 /**
- * Class StringHelper.
+ * Class StringHelper
  * @package davidhirtz\yii2\skeleton\helpers
  */
-class StringHelper extends \yii\helpers\BaseStringHelper
+class StringHelper extends BaseStringHelper
 {
+    /**
+     * Obfuscates email address for public display.
+     *
+     * @param string $email
+     * @param bool $obfuscateDomain
+     * @param int $length
+     * @param int $maxLength
+     * @param string $replacement
+     * @return string
+     */
+    public static function obfuscateEmail($email, $obfuscateDomain = true, $length = 2, $maxLength = 5, $replacement = '*')
+    {
+        $parts = explode('@', $email);
+        $url = explode('.', $parts[1]);
+
+        $email = static::obfuscateText($parts[0], $length, $maxLength, $replacement) . '@';
+
+        if ($obfuscateDomain) {
+            $email .= static::obfuscateText(array_shift($url), $length, $maxLength, $replacement) . '.';
+        }
+
+        return $email . implode('.', $url);
+    }
+
+    /**
+     * @param string $text
+     * @param int $length
+     * @param int $maxLength
+     * @param string $replacement
+     * @return string
+     */
+    public static function obfuscateText($text, $length = 2, $maxLength = 5, $replacement = '*')
+    {
+        $length = min($length, strlen($text));
+        return substr($text, 0, $length) . str_repeat($replacement, min(strlen($text) - $length, $maxLength));
+    }
 }
