748e0831de347e9c938c4edd6ff4676aa4b9e822
davidhirtz <david.hirtz@me.com>
2022-02-07 16:52:27 +0100

    Update update.php

--- modules/admin/views/account/update.php
+++ modules/admin/views/account/update.php
@@ -64,50 +64,50 @@ if ($user->isUnconfirmed()) {
 }
 ?>
 
-<?php Panel::begin([
-    'title' => Yii::t('skeleton', 'Clients'),
-]) ?>
+<?php if (Yii::$app->getAuthClientCollection()->clients) {
+    Panel::begin([
+        'title' => Yii::t('skeleton', 'Clients'),
+    ]);
 
-<?php
-if ($user->authClients) {
-    ?>
-    <table class="table table-vertical table-striped">
-        <thead>
-        <tr>
-            <th><?= Yii::t('skeleton', 'Client'); ?></th>
-            <th><?= Yii::t('skeleton', 'Name'); ?></th>
-            <th class="d-none d-table-cell-md"><?= Yii::t('skeleton', 'Updated'); ?></th>
-            <th class="d-none d-table-cell-lg"><?= Yii::t('skeleton', 'Created'); ?></th>
-            <th></th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php
-        foreach ($user->authClients as $auth) {
-            $client = $auth->getClientClass();
-            $url = $client::getExternalUrl($auth);
-            $title = $client->getTitle();
-            ?>
-            <tr>
-                <td><?= $title; ?></td>
-                <td><?= $url ? Html::a($auth->getDisplayName(), $url, ['target' => '_blank']) : $auth->getDisplayName(); ?></td>
-                <td class="d-none d-table-cell-md"><?= Timeago::tag($auth->updated_at); ?>
-                <td class="d-none d-table-cell-lg"><?= Timeago::tag($auth->created_at); ?>
-                <td class="text-right">
-                    <a href="<?= Url::to(['deauthorize', 'id' => $auth->id, 'name' => $auth->name]) ?>" data-method="post" data-confirm="<?= Yii::t('skeleton', 'Are you sure your want to remove your {client} account?', ['client' => $title]); ?>" data-toggle="tooltip" title="<?= Yii::t('skeleton', 'Remove {client}', ['client' => $title]); ?>" class="btn btn-danger">
-                        <?= Icon::tag('trash-alt'); ?>
-                    </a>
-                </td>
-            </tr>
-            <?php
-        }
+    if ($user->authClients) {
         ?>
-        </tbody>
-    </table>
-    <hr>
-    <?php
-}
-?>
+        <table class="table table-vertical table-striped">
+            <thead>
+            <tr>
+                <th><?= Yii::t('skeleton', 'Client'); ?></th>
+                <th><?= Yii::t('skeleton', 'Name'); ?></th>
+                <th class="d-none d-table-cell-md"><?= Yii::t('skeleton', 'Updated'); ?></th>
+                <th class="d-none d-table-cell-lg"><?= Yii::t('skeleton', 'Created'); ?></th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            foreach ($user->authClients as $auth) {
+                $client = $auth->getClientClass();
+                $url = $client::getExternalUrl($auth);
+                $title = $client->getTitle();
+                ?>
+                <tr>
+                    <td><?= $title; ?></td>
+                    <td><?= $url ? Html::a($auth->getDisplayName(), $url, ['target' => '_blank']) : $auth->getDisplayName(); ?></td>
+                    <td class="d-none d-table-cell-md"><?= Timeago::tag($auth->updated_at); ?>
+                    <td class="d-none d-table-cell-lg"><?= Timeago::tag($auth->created_at); ?>
+                    <td class="text-right">
+                        <a href="<?= Url::to(['deauthorize', 'id' => $auth->id, 'name' => $auth->name]) ?>" data-method="post" data-confirm="<?= Yii::t('skeleton', 'Are you sure your want to remove your {client} account?', ['client' => $title]); ?>" data-toggle="tooltip" title="<?= Yii::t('skeleton', 'Remove {client}', ['client' => $title]); ?>" class="btn btn-danger">
+                            <?= Icon::tag('trash-alt'); ?>
+                        </a>
+                    </td>
+                </tr>
+                <?php
+            }
+            ?>
+            </tbody>
+        </table>
+        <hr>
+        <?php
+    }
+    ?>
     <p>
         <?= Yii::t('skeleton', 'Click {here} to add {clientCount, plural, =0{an external client} other{additional clients}} to your account.', [
             'clientCount' => count($user->authClients),
@@ -117,7 +117,7 @@ if ($user->authClients) {
             ]),
         ]); ?>
     </p>
-<?php Panel::end(); ?>
+    <?php Panel::end(); ?>
     <div class="modal fade" id="auth-client-modal" tabindex="-1" role="dialog" aria-labelledby="resize-modal-label">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
@@ -147,6 +147,8 @@ if ($user->authClients) {
             </div>
         </div>
     </div>
+    <?php
+} ?>
 
 <?php
 if (!$user->isOwner()) {
