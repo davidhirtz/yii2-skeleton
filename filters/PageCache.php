563c3ac3f26ce6bb4d970cd2d225835cb8dcde26
davidhirtz <david.hirtz@me.com>
2023-07-20 07:12:55 +0200

    Create PageCache.php

---
+++ filters/PageCache.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace davidhirtz\yii2\skeleton\filters;
+
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\caching\TagDependency;
+
+class PageCache extends \yii\filters\PageCache
+{
+    public const TAG_DEPENDENCY_KEY = 'page-cache';
+
+    /**
+     * @var bool|null leave null to use default value
+     */
+    public $enabled = null;
+
+    /**
+     * @var string[]
+     */
+    public $only = ['index', 'view'];
+
+    /**
+     * @var array the default GET param values used by {@see static::$variations}
+     */
+    public array $params = [];
+
+    /**
+     * @return void
+     */
+    public function init(): void
+    {
+        $request = Yii::$app->getRequest();
+
+        $this->dependency ??= [
+            'class' => TagDependency::class,
+            'tags' => [self::TAG_DEPENDENCY_KEY],
+            'reusable' => !$this->cacheCookies,
+        ];
+
+        $this->enabled ??= Yii::$app->getRequest()->getIsGet() && Yii::$app->getUser()->getIsGuest();
+
+        if (!is_callable($this->variations)) {
+            $this->variations ??= [];
+            $this->variations[] = $request->getIsAjaxRoute();
+            $this->variations[] = Yii::$app->language;
+
+            foreach ($this->params as $param) {
+                $this->variations[] = $request->get($param, '');
+            }
+        } elseif ($this->params) {
+            throw new InvalidConfigException('PageCache::$params cannot be set if "variations" is callable.');
+        }
+
+        parent::init();
+    }
+}
