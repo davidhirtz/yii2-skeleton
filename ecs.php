c8342f49197c7b1d510686ad6119a7daad51dc69
davidhirtz <david.hirtz@me.com>
2023-11-08 10:20:10 +0100

    ECS

---
+++ ecs.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/app',
+    ]);
+
+    $ecsConfig->rules([
+        NoUnusedImportsFixer::class,
+    ]);
+
+    $ecsConfig->sets([
+        SetList::DOCBLOCK,
+        SetList::NAMESPACES,
+        SetList::COMMENTS,
+        SetList::PSR_12,
+    ]);
+};
