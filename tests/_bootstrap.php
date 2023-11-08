9c0b8b42e7af01523d2ad6c7da1bf1f152935e38
davidhirtz <david.hirtz@me.com>
2023-11-08 13:34:31 +0100

    PHPStan config

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -3,5 +3,5 @@
 defined('YII_ENV') or define('YII_ENV', 'test');
 defined('YII_APP_BASE_PATH') or define('YII_APP_BASE_PATH', __DIR__ . '/../../../');
 
-require_once __DIR__ . '/../../../../vendor/autoload.php';
-require_once __DIR__ . '/../../../../vendor/yiisoft/yii2/Yii.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
