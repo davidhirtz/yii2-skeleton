a7dc81dc5649977cfd885447e347fd1f7dfc189c
davidhirtz <david.hirtz@me.com>
2023-12-18 14:10:27 +0100

    Testing with MySQL DSN

--- tests/config/test.php
+++ tests/config/test.php
@@ -13,15 +13,11 @@ return [
             'linkAssets' => true,
         ],
         'db' => [
-            'dsn' => sprintf(
-                'mysql:host=%s;dbname=%s',
-                getenv('MYSQL_HOST') ?: '127.0.0.1',
-                getenv('MYSQL_DATABASE') ?: 'yii2_skeleton_test'
-            ),
+            'dsn' => getenv('MYSQL_DSN') ?: ('mysql:host=127.0.0.1;dbname=' . (getenv('MYSQL_DATABASE') ?: 'yii2_skeleton_test')),
             'username' => getenv('MYSQL_USER') ?: 'root',
             'password' => getenv('MYSQL_PASSWORD') ?: '',
             'charset' => 'utf8',
-            ...is_file(__DIR__ . '/db.php') ? require (__DIR__ . '/db.php') : [],
+            ...is_file(__DIR__ . '/db.php') ? require(__DIR__ . '/db.php') : [],
         ],
         'mailer' => [
             'useFileTransport' => true,
