82c40465e99eca1922793009d7d64ab86c5e6886
davidhirtz <david.hirtz@me.com>
2023-12-18 13:25:48 +0100

    Starting MySQL in workflow

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,12 +1,10 @@
 <?php
 
-use Codeception\Util\Autoload;
-
 defined('YII_ENV') or define('YII_ENV', 'test');
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
-Autoload::addNamespace('davidhirtz\yii2\skeleton\tests\support\_generated', __DIR__ . '/support/_generated/');
+//\Codeception\Util\Autoload::addNamespace('davidhirtz\yii2\skeleton\tests\support\_generated', __DIR__ . '/support/_generated/');
 
