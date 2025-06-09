d0c3a4d1cfc9775e19e91c5ddc2d09603915476f
davidhirtz <david.hirtz@me.com>
2025-06-09 09:05:56 +0200

    FontAwesome 6

--- src/assets/FontAwesomeAsset.php
+++ src/assets/FontAwesomeAsset.php
@@ -7,17 +7,10 @@ namespace davidhirtz\yii2\skeleton\assets;
 use yii\web\AssetBundle;
 
 /**
- * Includes FontAwesome 5.15.4 CSS and WOFF2 files, everything es is excluded.
+ * Includes FontAwesome 6.7.2 CSS and WOFF2 files, everything es is excluded.
  */
 class FontAwesomeAsset extends AssetBundle
 {
-    /**
-     * @var array
-     */
     public $css = ['css/all.css'];
-
-    /**
-     * @var string
-     */
     public $sourcePath = '@skeleton/assets/fontawesome';
 }
