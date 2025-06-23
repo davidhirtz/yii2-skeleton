4668503513949caff3df5c9da266ef54db431262
davidhirtz <david.hirtz@me.com>
2025-06-23 14:13:14 +0200

    Dropdowns

--- src/widgets/forms/DeleteActiveForm.php
+++ src/widgets/forms/DeleteActiveForm.php
@@ -64,7 +64,7 @@ class DeleteActiveForm extends ActiveForm
         $this->confirm ??= Yii::t('yii', 'Are you sure you want to delete this item?');
 
         $btn = Button::danger($this->label)
-            ->type('submit');
+            ->type('button');
 
         if ($this->confirm) {
             $modal = Modal::make()
