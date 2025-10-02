f4cbc7169a9a7553f407aa4a7598bcf590c7f9a3
davidhirtz <david.hirtz@me.com>
2025-10-02 15:31:12 +0200

    Update ConnectionTest.php

--- tests/unit/db/ConnectionTest.php
+++ tests/unit/db/ConnectionTest.php
@@ -7,6 +7,7 @@ namespace davidhirtz\yii2\skeleton\tests\unit\db;
 use Codeception\Test\Unit;
 use davidhirtz\yii2\skeleton\helpers\FileHelper;
 use Yii;
+use yii\helpers\Console;
 
 class ConnectionTest extends Unit
 {
@@ -16,12 +17,14 @@ class ConnectionTest extends Unit
         $db->maxBackups = 1;
 
         $filePath = $db->backup();
+        Console::stdout(PHP_EOL . " > Filepath: $filePath" . PHP_EOL);
 
         self::assertFileExists($filePath);
         self::assertStringContainsString('runtime/backups/yii2_skeleton_test', $filePath);
         self::assertStringEndsWith('.sql', $filePath);
 
         $newFilePath = $db->backup();
+        Console::stdout(PHP_EOL . "New Filepath: $newFilePath" . PHP_EOL);
 
         self::assertNotSame($filePath, $newFilePath);
         self::assertFileExists($newFilePath);
