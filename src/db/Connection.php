d84827f1a3a3703c03715f567b40b6b0da2e57e4
davidhirtz <david.hirtz@me.com>
2025-11-05 12:45:16 +0100

    Update Connection.php

--- src/db/Connection.php
+++ src/db/Connection.php
@@ -129,7 +129,12 @@ class Connection extends \yii\db\Connection
             $cmd->useExec = true;
         }
 
-        return $cmd->execute();
+        if (!$cmd->execute()) {
+            Yii::error($cmd->getStdErr(), __METHOD__);
+            return false;
+        }
+
+        return true;
     }
 
     public function getSchema(): Schema
