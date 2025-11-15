2d095f58e764b50ed2555079edb6e6fd77bf26d8
davidhirtz <david.hirtz@me.com>
2025-11-15 11:20:39 +0100

    Update LinkDataColumn.php

--- src/widgets/grids/columns/LinkDataColumn.php
+++ src/widgets/grids/columns/LinkDataColumn.php
@@ -4,23 +4,53 @@ declare(strict_types=1);
 
 namespace davidhirtz\yii2\skeleton\widgets\grids\columns;
 
-use yii\grid\DataColumn;
-use yii\helpers\Html;
+use Closure;
+use davidhirtz\yii2\skeleton\widgets\grids\GridView;
+use yii\base\Model;
+use yii\i18n\Formatter;
 
 class LinkDataColumn extends DataColumn
 {
-    /**
-     * @var callable|null a callback function that returns the route for the count link
-     */
-    public mixed $route = null;
-
-    /**
-     * @var array contains the HTML attributes for the link or wrapper
-     */
-    public array $wrapperOptions = [];
+    public function __construct(
+        GridView $grid,
+        Formatter $formatter,
+        ?string $attribute = null,
+        ?string $label = null,
+        protected array|string|null|Closure $route = null,
+        protected array $wrapperAttributes = [],
+        Closure|string|null $value = null,
+        string $format = 'text',
+        Closure|string|null $content = null,
+        array|Closure $contentAttributes = [],
+        ?string $header = null,
+        array $headerAttributes = [],
+        array $sortLinkAttributes = [],
+        bool $visible = true,
+        bool $enableSorting = true,
+        bool $encodeLabel = true,
+        string $emptyCell = '&nbsp;'
+    ) {
+        parent::__construct(
+            $grid,
+            $formatter,
+            $attribute,
+            $label,
+            $value,
+            $format,
+            $content,
+            $contentAttributes,
+            $header,
+            $headerAttributes,
+            $sortLinkAttributes,
+            $visible,
+            $enableSorting,
+            $encodeLabel,
+            $emptyCell,
+        );
+    }
 
     #[\Override]
-    protected function renderDataCellContent($model, $key, $index): string
+    protected function renderDataCellContent(Model $model, string|int $key, int $index): string|Stringable
     {
         $route = is_callable($this->route) ? call_user_func($this->route, $model) : $this->route;
         $content = parent::renderDataCellContent($model, $key, $index);
@@ -30,9 +60,9 @@ class LinkDataColumn extends DataColumn
         }
 
         if ($route) {
-            return Html::a($content, $route, $this->wrapperOptions);
+            return Html::a($content, $route, $this->wrapperAttributes);
         }
 
-        return $this->wrapperOptions ? Html::tag('div', $content, $this->wrapperOptions) : $content;
+        return $this->wrapperAttributes ? Html::tag('div', $content, $this->wrapperAttributes) : $content;
     }
 }
