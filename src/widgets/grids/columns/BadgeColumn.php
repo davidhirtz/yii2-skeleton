a0dcca93526a3b69d10ebe7ac80f70debd93e703
davidhirtz <david.hirtz@me.com>
2025-11-29 12:17:10 +0100

    Added `showEmpty` property

--- src/widgets/grids/columns/BadgeColumn.php
+++ src/widgets/grids/columns/BadgeColumn.php
@@ -5,12 +5,21 @@ declare(strict_types=1);
 namespace davidhirtz\yii2\skeleton\widgets\grids\columns;
 
 use Closure;
+use Yii;
 
 class BadgeColumn extends LinkColumn
 {
     public ?array $headerAttributes = ['class' => 'text-center'];
     public array|null|Closure $contentAttributes = ['class' => 'text-center'];
     public array $linkAttributes = ['class' => 'badge'];
+    public bool $showEmpty = false;
 
     public string $format = 'integer';
+
+    protected function formatValue(mixed $value): string
+    {
+        return $value || $this->showEmpty
+            ? Yii::$app->getFormatter()->format($value, $this->format)
+            : '';
+    }
 }
