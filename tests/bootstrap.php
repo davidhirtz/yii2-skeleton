56220d77f6fee58db3b70135f2a9127704552497
davidhirtz <david.hirtz@me.com>
2025-12-10 07:08:01 +0000

    Moved dev dependencies to monorepo

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,4 @@ declare(strict_types=1);
 defined('YII_ENV') or define('YII_ENV', 'test');
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
-
-Yii::setAlias('@tests', __DIR__);
+require((getenv('BASEPATH') ?: getcwd()) . '/vendor/yiisoft/yii2/Yii.php');
---
+++ tests/config.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'components' => [
+        'db' => [
+            'dsn' => getenv('MYSQL_DSN') ?: 'mysql:host=127.0.0.1;dbname=yii2_test',
+            'username' => getenv('MYSQL_USER') ?: 'root',
+            'password' => getenv('MYSQL_PASSWORD') ?: '',
+            'charset' => 'utf8',
+        ],
+    ],
+];
